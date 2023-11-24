6cdbfd39813b4433767e75d7be36155c79679d64
TheLazyBusyCoder <108222281+TheLazyBusyCoder@users.noreply.github.com>
2023-11-24 19:25:37 +0530

    Update typing.py

--- typing.py
+++ typing.py
@@ -6,8 +6,16 @@ import keyboard
 characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=~`[]{}|;:'\",.<>/?\\"
 
 def generate_random_string():
-    length = random.randint(1, 7)
-    return ''.join(random.choice(characters) for _ in range(length))
+    length = random.randint(5, 11)
+    output = ''.join(random.choice(characters) for _ in range(length))
+    
+    num_spaces = random.randint(1, 3)
+    positions = random.sample(range(1, length - 1), num_spaces) 
+
+    for pos in sorted(positions, reverse=True):
+        output = output[:pos] + ' ' + output[pos:]
+    
+    return output
 
 correct = 0
 
